e5520846f1898c4554ea0d8f8c0542d653d4b5c4
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-10 15:12:20 +1200

    Created vehicle class

---
+++ Java/com/FleetManager/Vehicle.java
@@ -0,0 +1,29 @@
+package com.FleetManager;
+
+public class Vehicle { 
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+    public String getMake(){return make;}
+
+    public void setMake(String make){this.make = make;}
+
+    public String getModel(){return model;}
+
+    public void setModel(String model){this.model = model;}
+
+    public int getYear(){return year;}
+    
+    public void setYear(int year){this.year = year;}
+}
