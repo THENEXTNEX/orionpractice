2b769abac13078ac22ffe1c5d02a525113cb05c2
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-10 16:45:13 +1200

    Created Media Item and their subclasses, begun media manager

---
+++ Java/com/MediaLibrary/MediaManager.java
@@ -0,0 +1,15 @@
+package com.MediaLibrary;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediaManager {
+    List<MediaItem> media;
+    
+    public static void main(String[] args){
+
+    }
+
+    public MediaManager(){
+        this.media = new ArrayList<>();
+    }
+}
