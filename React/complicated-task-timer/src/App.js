b8f64c03a86a4c0ef2e149d8e0a110c5b0c46d15
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-12 17:38:32 +1200

    Added filters and formatted the time to mm:ss

--- React/complicated-task-timer/src/App.js
+++ React/complicated-task-timer/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const [intervals, setIntervals] = useState({});
 
-  const [filter, setFilter] = useState("All")
+  const [filter, setFilter] = useState("All");
 
   const addTask = () => {
     if (taskText.trim() === "") return;
@@ -42,6 +42,11 @@ function App() {
     setTaskList(newList);
   };
 
+  const formatTime = (totalSeconds) => {
+    const minutes = String(Math.floor(totalSeconds/60)).padStart(2, '0');
+    const seconds = String(totalSeconds % 60).padStart(2,'0');
+    return `${minutes}:${seconds}`;
+  }
   return (
     <div style={{ marginLeft: "16px" }}>
       <h1>Task Timers</h1>
@@ -55,11 +60,24 @@ function App() {
         </input>
         <button onClick={addTask}>Add Task</button>
       </h2>
-
+      <h3>Select Filter: 
+        <select
+        onChange={(e) => setFilter(e.target.value)} 
+        value={filter}
+        style={{ marginLeft: "8px" }}>
+          <option value={"All"}>All</option>
+          <option value={"Running"}>Running</option>
+          <option value={"Stopped"}>Stopped</option>
+        </select>
+      </h3>
       <ul>
-        {taskList.map((tasks, index) => (
+        {taskList.filter((task) => {
+          if(filter === "All") return true;
+          if(filter === "Running") return task.isRunning;
+          if(filter === "Stopped") return !task.isRunning;
+        }).map((tasks, index) => (
           <li key={index}>
-            {tasks.text} - {tasks.time}<button onClick={() => startTimer(index)} style={{ marginLeft: "16px" }}> Start </button> <button onClick={() => stopTimer(index)}>Stop</button>
+            {tasks.text} - {formatTime(tasks.time)}<button onClick={() => startTimer(index)} style={{ marginLeft: "16px" }}> Start </button> <button onClick={() => stopTimer(index)}>Stop</button>
           </li>
         ))}
       </ul>
