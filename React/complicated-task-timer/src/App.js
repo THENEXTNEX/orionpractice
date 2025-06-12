222ac59e9c9a38199b1e6b84c4daf91db5a9a3b5
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-12 17:24:01 +1200

    Created timers for each list item

--- React/complicated-task-timer/src/App.js
+++ React/complicated-task-timer/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [taskList, setTaskList] = useState([]);
+
+  const [taskText, setTaskText] = useState("");
+
+  const [intervals, setIntervals] = useState({});
+
+  const [filter, setFilter] = useState("All")
+
+  const addTask = () => {
+    if (taskText.trim() === "") return;
+    setTaskList([...taskList, { text: taskText, time: 0, isRunning: false }]);
+    setTaskText("");
+  }
+
+  const startTimer = (index) => {
+    if (taskList[index].isRunning) return; // Already running
+
+    const newList = [...taskList];
+    newList[index].isRunning = true;
+    setTaskList(newList);
+
+    const id = setInterval(() => {
+      setTaskList((prevList) => {
+        const updated = [...prevList];
+        updated[index] = { ...updated[index], time: updated[index].time + 1 }
+        return updated;
+      });
+    }, 1000);
+
+    setIntervals((prev) => ({ ...prev, [index]: id }));
+  };
+
+  const stopTimer = (index) => {
+    clearInterval(intervals[index]);
+
+    const newList = [...taskList];
+    newList[index].isRunning = false;
+    setTaskList(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: "16px" }}>
+      <h1>Task Timers</h1>
+
+      <h2>Task:
+        <input
+          value={taskText}
+          style={{ marginLeft: "16px", marginRight: "16px" }}
+          placeholder="Enter New Task Here"
+          onChange={(e) => setTaskText(e.target.value)}>
+        </input>
+        <button onClick={addTask}>Add Task</button>
+      </h2>
+
+      <ul>
+        {taskList.map((tasks, index) => (
+          <li key={index}>
+            {tasks.text} - {tasks.time}<button onClick={() => startTimer(index)} style={{ marginLeft: "16px" }}> Start </button> <button onClick={() => stopTimer(index)}>Stop</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
