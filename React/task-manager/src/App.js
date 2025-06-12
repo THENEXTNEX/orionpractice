e738d3908532a9b9634578ca254ae4d6baa518ae
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-12 18:26:49 +1200

    Can enter in new tasks and have it appear as a list, to add amounts and categories

--- React/task-manager/src/App.js
+++ React/task-manager/src/App.js
@@ -2,9 +2,38 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="Task Manager">
 
+  const[tasks, setTasks] = useState([]);
+
+  const[newTaskText, setTaskText] = useState("");
+
+  const addTask = () => {
+    if(newTaskText.trim() === "") return;
+    setTasks([...tasks, {text: newTaskText}]);
+    setTaskText("");
+  }
+
+  return (
+    <div style={{marginLeft: "16px"}}className="Task Manager">
+      <h1>Task Manager</h1>
+      <h2>
+        Task:
+        <input style={{marginLeft : "16px", marginRight: "16px"}}
+          value={newTaskText}
+          placeholder="Enter new task here..."
+          onChange={(e) => setTaskText(e.target.value)}>
+        </input>
+
+        <button onClick={addTask}>Add Task</button>
+      </h2>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
