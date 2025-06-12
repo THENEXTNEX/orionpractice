f97c23675b6f43fa180577afa44df74dda71b622
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-13 10:38:04 +1200

    Created expense.java

---
+++ Java/com/ExpenseManager/Expense.java
@@ -0,0 +1,25 @@
+package com.ExpenseManager;
+
+public class Expense {
+    private String description;
+    private double amount;
+    private String category;
+
+    public Expense(String description, double amount, String category){
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public String getDescription(){return description;}
+
+    public void setDescription(String description){this.description = description;}
+
+    public double getAmount(){return amount;}
+
+    public void setAmount(double amount){this.amount = amount;}
+
+    public String getCategory(){return category;}
+
+    public void setCategory(String category){this.category = category;}
+}
