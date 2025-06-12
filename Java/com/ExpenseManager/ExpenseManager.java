d0f171b438d6cebcf3ef74d851cf147f4e14ee27
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-13 11:38:43 +1200

    Placed in while loop so multiple expenses can be added, can be listed out now

--- Java/com/ExpenseManager/ExpenseManager.java
+++ Java/com/ExpenseManager/ExpenseManager.java
@@ -17,57 +17,61 @@ public class ExpenseManager {
         double amount = 0.0;
         String category = "";
 
-        System.out.println("Welcome to the expense tracker! Please select from the following!");
-        System.out.println("1. Add Expense\n2. View All Expenses\n3. View by Category\n4. Show Summary\n5. Exit");
-        switch (scanner.nextInt()) {
-            case 1:
-                scanner.nextLine();
-                while (true) {
-                    System.out.println("Please enter expense name: ");
-                    name = scanner.nextLine();
 
-                    if (name.trim().isEmpty()) {
-                        System.out.println("Expense name can not be empty. Please try again.");
-                    } else {
-                        System.out.println("Valid name accepted!");
-                        break;
-                    }
-                }
+        while (true) {
+            System.out.println("Welcome to the expense tracker! Please select from the following!");
+            System.out.println("1. Add Expense\n2. View All Expenses\n3. View by Category\n4. Show Summary\n5. Exit");
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("Adding Expense...");
+                    scanner.nextLine();
+                    while (true) {
+                        System.out.println("Please enter expense name: ");
+                        name = scanner.nextLine();
 
-                while (true) {
-                    System.out.println("Please enter the amount: ");
-                    try {
-                        amount = scanner.nextDouble();
-                        System.out.println("Valid amount accepted!");
-                        scanner.nextLine();
-                        break;
-                    } catch (InputMismatchException e) {
-                        System.out.println("Invalid number. Please try again.");
-                        scanner.nextLine();
+                        if (name.trim().isEmpty()) {
+                            System.out.println("Expense name can not be empty. Please try again.");
+                        } else {
+                            System.out.println("Valid name accepted!");
+                            break;
+                        }
                     }
 
-                }
-                while (true) {
-                    System.out.println("Please enter the category \"Food\", \"Travel\", or \"Personal\": ");
-                    category = scanner.nextLine();
-                    if (isValidCategory(category)) {
-                        System.out.println("Valid category accepted!");
-                        break;
-                    } else {
-                        System.out.println("Please enter a category of \"Food\", \"Travel\", or \"Personal\".");
-                        scanner.nextLine();
+                    while (true) {
+                        System.out.println("Please enter the amount: ");
+                        try {
+                            amount = scanner.nextDouble();
+                            System.out.println("Valid amount accepted!");
+                            scanner.nextLine();
+                            break;
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid number. Please try again.");
+                            scanner.nextLine();
+                        }
+
                     }
-                }
+                    while (true) {
+                        System.out.println("Please enter the category \"Food\", \"Travel\", or \"Personal\": ");
+                        category = scanner.nextLine();
+                        if (isValidCategory(category)) {
+                            System.out.println("Valid category accepted!");
+                            break;
+                        } else {
+                            System.out.println("Please enter a category of \"Food\", \"Travel\", or \"Personal\".");
+                            scanner.nextLine();
+                        }
+                    }
+                    Expense expense1 = new Expense(name, amount, category);
+                    expenseM.addExpense(expense1);
+                    System.out.println("Expense Added!");
+                    break;
 
-                scanner.close();
-
-                Expense expense1 = new Expense(name, amount, category);
-                expenseM.addExpense(expense1);
-                System.out.println("Expense Added!");
-                break;
+                case 2:
+                    scanner.nextLine();
+                    expenseM.listExpenses();
+            }
         }
-
-        //expenseM.listExpenses();
+        
     }
 
     public static boolean isValidCategory(String category) {
@@ -83,6 +87,9 @@ public class ExpenseManager {
     }
 
     public void listExpenses() {
+        if (expenseList.isEmpty()) {
+            System.out.println("No expense added yet, please add them in the first menu!");
+        }
         for (Expense e : expenseList) {
             System.out.println(e);
         }
