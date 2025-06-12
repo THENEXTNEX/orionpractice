d6d71f7901e6bbe9fbfacf88d78673cb1ad952fb
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-12 16:57:28 +1200

    Added button functionality and have is the number is even or odd

--- React/step-counter/src/App.js
+++ React/step-counter/src/App.js
@@ -16,13 +16,25 @@ function App (){
     setStepText("");
   }
 
+  const removeStep = () => {
+    if(stepText.trim() === "") return;
+    const parsed = parseInt(stepText);
+    if(!isNaN(parsed)){
+      setCount(prev => prev - parsed);
+    }
+    setStepText("");
+  }
 
+  const reset = () => {
+    setCount(0);
+  }
   return (
     <div>
 
     <h1 style={{marginLeft: "16px"}}>Step Counter App</h1>
 
     <h2 style={{marginLeft: "16px"}}>Current Count: {count}</h2>
+    <h2 style={{marginLeft: "16px"}}>This number is {count % 2 === 0 ? "Even" : "Odd"}</h2>
     <h2 style={{marginLeft: "16px"}}>Step: 
       <input
       style={{marginLeft: "16px"}}
@@ -30,12 +42,10 @@ function App (){
       onChange={(e) => setStepText(e.target.value)}
       placeholder="Enter Amount of steps here"
       ></input>
-      <button onClick={addStep}>Add Step</button>
-
     <p>
-      <button> + </button>
-      <button style={{marginLeft: "16px"}}> - </button>
-      <button style={{marginLeft: "16px"}}> reset </button>
+      <button onClick={addStep}> + </button>
+      <button onClick={removeStep} style={{marginLeft: "16px"}}> - </button>
+      <button onClick={reset} style={{marginLeft: "16px"}}> reset </button>
     </p>
     </h2>
     
