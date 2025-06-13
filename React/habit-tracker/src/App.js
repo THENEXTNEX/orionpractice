d012d97114975c66f1963a28b0419939533543e4
Caleb Simmons <calebsimmons39@gmail.com>
2025-06-13 13:29:35 +1200

    Added basic formatting with user inputs, category drop down, displaying habit list information

--- React/habit-tracker/src/App.js
+++ React/habit-tracker/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  //Habit list
+  const[habit, setHabit] = useState([]);
+
+  //Habit Text
+  const[habitText, setHabitText] = useState("");
+
+  //Category
+  const[category, setCategory] = useState("Health");
+  //Adding the habit to the habit state
+  const addHabit = () => {
+    if(habitText.trim() === "")return;
+    setHabit([...habit, {text: habitText, category: category, isDone: false}]);
+    setHabitText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginLeft: "16px"}} className="Habit Tracker App">
+      <h1>Habit Tracking App</h1>
+      <h2>Enter Habit here:
+        <input
+        style={{marginLeft: "8px"}}
+        value={habitText}
+        placeholder="Enter Habit here..."
+        onChange={(e) => setHabitText(e.target.value)}>
+        </input>
+        <select onChange={(e) => setCategory(e.target.value)} style={{marginLeft: "8px"}} value={category}>
+          <option value={"Health"}>Health</option>
+          <option value={"Productivity"}>Productivity</option>
+          <option value={"Mindfulness"}>Mindfulness</option>
+        </select>
+        <button onClick={addHabit} style={{marginLeft: "8px"}}>Add Habit</button>
+        </h2>
+      <ul>
+        {habit.map((habits, index) => (
+          <li key={index}>
+            Habit: {habits.text} - Type: {habits.category}
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
